8c4c85d03e64e836ed6cefa3ebc2efd0cf823b14
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-23 13:35:58 +0300

    added get user/update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -65,7 +65,12 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access   Public
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout user" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
 });
 
 //@desc     Get user profile
@@ -73,7 +78,13 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@access   Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+
+  res.status(200).json(user);
 });
 
 //@desc     Update user profile
@@ -81,6 +92,28 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //@access   Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
   res.status(200).json({ message: "Update user profile" });
 });
 
