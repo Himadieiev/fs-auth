d75bc8741fd8d61de83c783336d126bd35e8707d
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-23 11:28:53 +0300

    added mongo db setup/model

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
