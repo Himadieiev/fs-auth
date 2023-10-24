17fa08fc9dd4325a4a0eee4ebf6621cac221edc6
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-24 11:29:04 +0300

    added login logic/toast

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
     <div className="py-5">
       <Container className="d-flex justify-content-center">
         <Card className="p-5 d-flex flex-column align-items-center hero-card bg-light w-75">
-          <h1 className="text-center mb-4">Authentication</h1>
+          <h1 className="text-center mb-4">MERN Authentication</h1>
           <p className="text-center mb-4">
-            This is a boilerplate for authentication that stores a JWT in an
-            HTTP-Only cookie. It also uses Redux Toolkit and the React Bootstrap
-            library
+            This is a boilerplate for MERN authentication that stores a JWT in
+            an HTTP-Only cookie. It also uses Redux Toolkit and the React
+            Bootstrap library
           </p>
           <div className="d-flex">
             <LinkContainer to="/login">
